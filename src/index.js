cd54e674ef7f4557e1d617162b6a9f4516373b4d
[Github <55690712+soonmac@users.noreply.github.com>
2022-02-24 22:20:17 +0900

    browserRouter basename

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import { BrowserRouter } from '../node_modules/react-router-dom/index';
 
 ReactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <App />
   </BrowserRouter>,
   document.getElementById('root')
